22b74c0ab600eb73b550a27bbfaa25fb0acd1f70
ckc2007 <ckc2007@gmail.com>
2023-07-23 19:06:54 -0400

    login_user mutation

--- client/src/graphql/mutations.js
+++ client/src/graphql/mutations.js
@@ -16,13 +16,22 @@ export const ADD_USER = gql`
 `;
 
 export const LOGIN_USER = gql`
-  mutation addUser($username: String!, $email: String!, $password: String!) {
-    addUser(username: $username, email: $email, password: $password) {
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
       token
       user {
         _id
         username
         email
+        savedBooks {
+          _id
+          authors
+          description
+          bookId
+          image
+          link
+          title
+        }
       }
     }
   }
