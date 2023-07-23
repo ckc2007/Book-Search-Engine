00ad69c06ca1e8a00f349a2af2daa7f83225d654
ckc2007 <ckc2007@gmail.com>
2023-07-23 19:01:19 -0400

    added login_user mutation

--- client/src/graphql/mutations.js
+++ client/src/graphql/mutations.js
@@ -1,10 +1,20 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
-export const LOGIN_USER = gql
+export const LOGIN_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
 
-export const ADD_USER = gql
+export const ADD_USER = gql;
 
-export const SAVE_BOOK = gql
+export const SAVE_BOOK = gql;
 
-export const REMOVE_BOOK = gql
-
+export const REMOVE_BOOK = gql;
