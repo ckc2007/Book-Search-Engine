4218262051d11547d88c4f61edfd6aa800fe2c3c
ckc2007 <ckc2007@gmail.com>
2023-07-23 15:29:44 -0400

    plug in queries and mutations into api

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,4 +1,11 @@
 import { gql, ApolloClient, InMemoryCache } from "@apollo/client";
+import { GET_ME } from "./graphql/queries";
+import {
+  LOGIN_USER,
+  ADD_USER,
+  SAVE_BOOK,
+  REMOVE_BOOK,
+} from "./graphql/mutations";
 
 const client = new ApolloClient({
   uri: "/graphql",
